748d541e7e07b4008c38ba02b0138f7144e13205
monezr <ramongomezrecio@gmail.com>
2017-09-07 10:24:28 +0200

    Update dotcode.py

--- dotcode.py
+++ dotcode.py
@@ -2,10 +2,10 @@ import tweepy
 
 from tweepy import OAuthHandler
 
-consumer_key = ' 5fB1XI3rJEvGbvWmJ1HK0ddhh'
-consumer_secret = ' jsS3sZOW1PDg1KBdtffAVD6uC43hejX6v4Lc245p5RYbAFcaeT'
-access_token = '1060794506-xoq3yDN6WY1OXrQqXTBTA7ZwjZ2s18dZ6MopxeF'
-access_secret = 'QaUeRj9Zw1pUTURn3JMVXYfBTBb0hEBTir6ulEu97yzA4'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_secret = ''
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token,access_secret)
